b7ad4589c20c731f5d6e771b1ae70f852d7cf11b
Phuc Luong <phuc.luong310@gmail.com>
2019-03-01 00:45:55 +0200

    Initial commit

---
+++ app/src/main/java/quiz/sample/com/mycount/MainActivity.java
@@ -0,0 +1,21 @@
+package quiz.sample.com.mycount;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Chronometer simpleCount = (Chronometer) findViewById(R.id.simpleCount);
+                simpleCount.start();
+    }
+}
